c8f64e2e2a05a191411f122dc5fdf03f0b8062a4
TLink666 <wangyixin99069@163.com>
2026-06-18 17:51:45 +0800

    add config

---
+++ src/config.py
@@ -0,0 +1,19 @@
+HF_CACHE = r"G:\huggingface_cache"
+
+OLLAMA_URL = "http://localhost:11434"
+
+CHUNK_SIZE = 200
+
+OVERLAP = 50
+
+TOP_K = 5
+
+HYBRID_ALPHA = 0.7
+
+EMBED_MODEL = "all-MiniLM-L6-v2"
+
+LLM_MODEL = "qwen2.5:7b"
+
+DATA_DIR = "../data"
+
+STORAGE_DIR = "../storage"
--- src/rag/generate.py
+++ src/rag/generate.py
@@ -1,10 +1,11 @@
 import requests
 from src.rag.prompt import build_prompt
+from src.config import *
 
 def ask_llm(context, question):
     prompt = build_prompt(context, question)
     res = requests.post(
-        "http://localhost:11434/api/generate",
+        f"{OLLAMA_URL}/api/generate",
         json={
             "model": "qwen2.5:7b",
             "prompt": prompt,
